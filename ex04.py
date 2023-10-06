6a78ddd61827c32f8a5670bef7eafb1628592bc4
AllanPatrick02 <allan.miranda@estudante.ifmt.edu.br>
2023-10-06 10:28:35 -0400

    ex04.py

--- ex04.py
+++ ex04.py
@@ -27,13 +27,33 @@ def q01():
         print('Valor não encontrado na lista')
     except:
         print('Erro desconhecido, contate o administrador do sistema!')
-q01()
 
 #2. Faça um programa que armazene 10 letras em uma lista e imprima uma listagem
 #numerada.
+def q02():
+
+    lista = []
+    
+    for x in range(10):
+        lista.append(chr(random.randrange(65,91)))
+    cont = 0
+    
+    for letra in lista:
+     print(f'{cont}: {letra}')
+     cont+= 1
 
 #3. Construa uma programa que armazene 15 números em uma lista e imprima
 #uma listagem numerada contendo o número e uma das mensagens: par ou ímpar.
+def q03():
+    lista = []
+    for x in range(15):
+        lista.append(random.randrange(100))
+    cont= 0
+    for c in lista:
+        print(f'{cont}: {c} - ')
+
+
+
 
 #4. Faça um programa que armazene 8 números em uma lista e imprima todos os
 #números. Ao final, imprima o total de números múltiplos de seis.
